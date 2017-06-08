40a0174b815924798779a85eadb6d7172d9dfb90
DhilipVelumani <dhilip.kumar444@gmail.com>
2017-06-08 11:28:52 +0530

    Create Party.java

---
+++ Party.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class Party
+{
+    public static void main(String arr[])
+    {
+        int num,i,j,count=1,flag=0;
+        
+        Scanner d=new Scanner(System.in);
+        num=d.nextInt();
+       
+        int array[]=new int[num];
+        for(i=0;i<array.length;i++)
+        {
+        
+           array[i]=d.nextInt();
+          
+        } 
+      
+        for(i=0;i<array.length;i++)
+        {
+            for(j=0;j<array.length;j++)
+            {
+                if(array[i]==array[j]&&i!=j)
+                {
+                       System.out.println(array[i]);
+                       count=0;
+                }
+                
+            }
+    
+        }
+        if(count!=0)
+        {
+             System.out.println("no repated numbers in array");
+        }
+       
+    }
+}
+
